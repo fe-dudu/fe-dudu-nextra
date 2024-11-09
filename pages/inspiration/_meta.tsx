6194a124c112cdcb8b538ce20dbfde10b6a56771
fe-dudu <ehehwhdwhd@naver.com>
2024-11-09 23:42:18 +0900

    Docs: resume, coworker

--- pages/inspiration/_meta.tsx
+++ pages/inspiration/_meta.tsx
@@ -1,9 +1,11 @@
 export default {
+  coworker: {
+    title: '등을 믿고 맡길 수 있는 동료',
+  },
   'nonviolent-communication': {
-    title: '비폭력 대화'
+    title: '비폭력 대화',
   },
   trippy: {
     title: 'Trippy',
   },
-  
 }
