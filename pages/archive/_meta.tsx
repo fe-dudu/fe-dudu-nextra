bd2aaf960a561534ae1debfa0e072127e7416c3a
fe-dudu <ehehwhdwhd@naver.com>
2024-11-17 01:54:00 +0900

    Docs: archive

--- pages/archive/_meta.tsx
+++ pages/archive/_meta.tsx
@@ -2,7 +2,13 @@ export default {
   'react-declarative-programming': {
     title: 'React declarative programming',
   },
+  mac: {
+    title: 'Mac setting',
+  },
   iterm2: {
     title: 'Iterm2 setting',
+  },
+  vscode: {
+    title: 'VSCode setting',
   },
 }
