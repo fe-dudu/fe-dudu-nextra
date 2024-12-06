6028f961fc77762ee225f91ce9b8c8b0994debeb
fe-dudu <ehehwhdwhd@naver.com>
2024-12-06 21:30:17 +0900

    docs: react reconciler flag

--- pages/blog/react/reconciler/_meta.tsx
+++ pages/blog/react/reconciler/_meta.tsx
@@ -1,12 +1,19 @@
 export default {
-  lane: {
-    title: 'Lane',
+  'fiber-root': {
+    title: 'FiberRoot',
+    display: 'hidden'
   },
   'fiber-architecture': {
     title: 'Fiber architecture',
   },
   fiber: {
     title: 'Fiber',
+  },
+  lane: {
+    title: 'Lane',
+  },
+  flag: {
+    title: 'Flag',
   },
   worktag: {
     title: 'WorkTag',
