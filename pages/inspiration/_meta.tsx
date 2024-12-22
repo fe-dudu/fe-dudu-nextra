0189e053a8761ef2c545040ec57f4775496aa45d
fe-dudu <ehehwhdwhd@naver.com>
2024-12-22 23:06:53 +0900

    docs: 2nd switch jobs

--- pages/inspiration/_meta.tsx
+++ pages/inspiration/_meta.tsx
@@ -1,12 +1,15 @@
 export default {
+  '2nd-switch-jobs': {
+    title: '💭 2번째 이직',
+  },
   'happy-moment': {
-    title: '💡 행복한 순간',
+    title: '💭 행복한 순간',
   },
   'peer-pressure': {
-    title: '💡 Peer pressure',
+    title: '💭 Peer pressure',
   },
   coworker: {
-    title: '💡 등을 믿고 맡길 수 있는 동료',
+    title: '💭 등을 믿고 맡길 수 있는 동료',
   },
   'nonviolent-communication': {
     title: '📖 비폭력 대화',
